c59411b241b9206e4f6ff27c16d38a5fa63e90dd
MrMoon127 <sasha.andia@gmail.com>
2023-10-01 22:00:32 -0700

    changed to use useEffect but it cannot run initially, needs a state based change first

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,6 +1,7 @@
-import React, {useReducer, useState} from "react";
+import React, {useReducer, useState, useEffect} from "react";
 
 const useApplicationData = () => {
+  
   const ACTIONS = {
     FAV_PHOTO_TOGGLE: 'FAV_PHOTO_TOGGLE',
     SET_PHOTO_DATA: 'SET_PHOTO_DATA',
@@ -8,20 +9,37 @@ const useApplicationData = () => {
     SELECT_PHOTO: 'SELECT_PHOTO',
     TOGGLE_MODAL: 'TOGGLE_MODAL'
   }
+  
+  const [favPhotoIds, toggleFav] = useReducer(reducer, []);
+  const [selectedPhoto, selectPhoto] = useReducer(reducer, 0);
+  const [openModal, toggleModal] = useReducer(reducer, false);
+  const [photoData, setPhotoData] = useReducer(reducer, []);
+  const [topicData, setTopicData] = useReducer(reducer, []);
+  
+  useEffect(() => {
+    fetch("/api/photos")
+      .then((response) => response.json())
+      .then((data) => setPhotoData({ type: ACTIONS.SET_PHOTO_DATA, payload: data }))
+  }, []);
+
+  useEffect(() => {
+    fetch("/api/topics")
+      .then((res) => res.json())
+      .then((data) => setTopicData({ type: ACTIONS.SET_TOPIC_DATA, payload: data }))
+  }, [])
 
   function reducer(state, action) {
     switch (action.type) {
       case 'FAV_PHOTO_TOGGLE':
-        console.log(state)
         if (state.includes(action.id)) {
           const filtered = state.filter((favPhotoId) => favPhotoId !== action.id);
           return filtered;
         }
         return [...state, action.id];
       case 'SET_PHOTO_DATA':
-        return action.data;
+        return { ...state, photoData: action.payload };
       case 'SET_TOPIC_DATA':
-        return action.data;
+        return { ...state, topicData: action.payload };
       case 'SELECT_PHOTO':
         return action.array.find((photo) => (photo.id === action.id));
       case 'TOGGLE_MODAL':
@@ -32,18 +50,16 @@ const useApplicationData = () => {
         );
     }
   }
-
-  const [favPhotoIds, toggleFav] = useReducer(reducer, []);
-  const [selectedPhoto, selectPhoto] = useReducer(reducer, 0);
-  const [openModal, toggleModal] = useReducer(reducer, false);
-
+  
   const state = {
     ACTIONS,
     selectedPhoto,
     openModal,
-    favPhotoIds
+    favPhotoIds,
+    photoData,
+    topicData
   };
-
+  
   return {
     state,
     selectPhoto,
